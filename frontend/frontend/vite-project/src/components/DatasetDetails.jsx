1fbdd5f0d8d5f0201de12e48f5eb33e0b5b09eaf
Jahnavi18104 <jahnavimeesala333@gmail.com>
2025-03-19 08:48:52 +0530

    Update DatasetDetails.jsx

--- frontend/frontend/vite-project/src/components/DatasetDetails.jsx
+++ frontend/frontend/vite-project/src/components/DatasetDetails.jsx
@@ -280,7 +280,7 @@ import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
 
-const API_URL = "http://localhost:5000"; // Backend URL
+const API_URL = "https://versel-recler.onrender.com"; // Backend URL
 
 const DatasetDetails = () => {
   const { id } = useParams();
