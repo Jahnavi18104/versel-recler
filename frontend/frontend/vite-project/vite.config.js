e850a73b52eb23f154396071ab409ce730acafac
Jahnavi18104 <jahnavimeesala333@gmail.com>
2025-03-21 23:22:07 +0530

    Update vite.config.js

--- frontend/frontend/vite-project/vite.config.js
+++ frontend/frontend/vite-project/vite.config.js
@@ -5,7 +5,11 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: '0.0.0.0',   // Listen on all interfaces
+    host: '0.0.0.0',  // Listen on all interfaces
     port: process.env.PORT || 5173,  // Use the Render-provided PORT or fallback to 5173
+    allowedHosts: [
+      'versel-recler-9.onrender.com', // Allow this host
+      'localhost', // Allow localhost (for local development)
+    ],
   },
 })
