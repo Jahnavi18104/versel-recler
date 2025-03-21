21bacd20f98dd5cac634441b5626f1cef60419c8
Jahnavi18104 <jahnavimeesala333@gmail.com>
2025-03-21 23:20:00 +0530

    Update vite.config.js

--- frontend/frontend/vite-project/vite.config.js
+++ frontend/frontend/vite-project/vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0',   // Listen on all interfaces
+    port: process.env.PORT || 5173,  // Use the Render-provided PORT or fallback to 5173
+  },
 })
